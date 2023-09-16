57492273d84eda13cb4843b65fc4f85d10853e84
lucas richards dev <lucasrichardsdev@gmail.com>
2023-09-16 11:12:16 -0700

    pull

--- main_app/models.py
+++ main_app/models.py
@@ -13,13 +13,13 @@ STATUS = (
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
-    due_date = models.DateField(default=now)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    # one letter to represent the meal Breakfast Lunch Dinner
-    status = models.CharField(
-      max_length=1,
-      #add choices field option that creates drop down
-      choices=STATUS,
-      default=STATUS[0][0]
-    )
-    # tasks = models.ForeignKey(Task, on_delete=models.CASCADE)
+    # due_date = models.DateField(default=now)
+    # owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    # # one letter to represent the meal Breakfast Lunch Dinner
+    # status = models.CharField(
+    #   max_length=1,
+    #   #add choices field option that creates drop down
+    #   choices=STATUS,
+    #   default=STATUS[0][0]
+    # )
+    # # tasks = models.ForeignKey(Task, on_delete=models.CASCADE)
