85d9eb397980923503f88a57584cc336815fc7f8
WahidSiddiqi <WahidSiddiqi@yahoo.com>
2023-09-16 21:47:33 -0700

    Finished Project model, has CRUD

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.utils.timezone import now
+from datetime import datetime, timedelta
 
 STATUS = (
     ('P', 'In Process'),
@@ -9,14 +10,28 @@ STATUS = (
     ('C', 'Completed')
 )
 
+
 # Comment
+
+
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=100)
     created_date = models.DateTimeField(auto_now_add=True)
 
-# Project
+
 class Project(models.Model):
+
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
-    
+    three_months_future = datetime.now() + timedelta(days=90)
+    due_date = models.DateField(default=three_months_future)
+    # owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    # one letter to represent the meal Breakfast Lunch Dinner
+    status = models.CharField(
+        max_length=1,
+        # add choices field option that creates drop down
+        choices=STATUS,
+        default=STATUS[0][0]
+    )
+    # tasks = models.ForeignKey(Task, on_delete=models.CASCADE)
