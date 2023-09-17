89ffeb429330669156a01f543da5571600c4e5ba
lucas richards dev <lucasrichardsdev@gmail.com>
2023-09-17 11:48:41 -0700

    merge successful profile and projects working

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,10 +10,8 @@ urlpatterns = [
     path('projects/', views.ProjectList.as_view(), name='projects_index'),
     path('projects/<int:pk>/', views.ProjectDetail.as_view(), name='projects_detail'),
     path('projects/create/', views.ProjectCreate.as_view(), name='projects_create'),
-    path('projects/<int:pk>/update/',
-         views.ProjectUpdate.as_view(), name='projects_update'),
-    path('projects/<int:pk>/delete/',
-         views.ProjectDelete.as_view(), name='projects_delete'),
+    path('projects/<int:pk>/update/',views.ProjectUpdate.as_view(), name='projects_update'),
+    path('projects/<int:pk>/delete/',views.ProjectDelete.as_view(), name='projects_delete'),
     # TASKS
     # COMMENTS
     # REGISTRATION
